757aa4b17e61d8b1bb65df0ec15b55d8012abde3
Kelvin <152228723+R-Kelvin@users.noreply.github.com>
2025-12-29 17:33:42 -0300

    update 0.1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+@section('content')
+
+<a href="{{ route ('login.index') }}">login</a>
+<h2>Home</h2>
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomerController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[HomerController::class,'index'])->name('home');
+
+Route::controller(LoginController::class)->group(function(){
+    Route::get('/login','index')->name('login.index');
+    Route::post('/login','store')->name('login.store');
+    Route::get('/logout','destroy')->name('login.destroy');
 });
