4e2868f56bf2e4772896c907d113bc997a652513
Kelvin <152228723+R-Kelvin@users.noreply.github.com>
2025-12-29 22:35:06 -0300

    update 0.2.3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomerController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomerController::class,'index'])->name('home');
